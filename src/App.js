cc6bea2d4734ccac54e80e1ccc3c4ae7476f2b9f
gsaikumar0625 <gsaikumar.empover@gmail.com>
2023-10-20 14:57:38 +0530

    Tetsting the changes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import useSWR from 'swr'
 import { fetcher } from './util';
 
 const App = () => { 
-  const { data, error } = useSWR('https://api.cortevaseedresearch.in/rest/mcrc/getCategoryMaster', fetcher,{ refreshInterval  : 1000 });   
-  if (error) {
-    return <div>Error loading data</div>;
-  } 
-  if (!data) {
-    return <div>Loading...</div>;
-  }  
+   
   return (
     <div>
-    <h1>User List</h1>
-    <select>
-      {data.response.productCategory.map((product) => ( 
-         <option key={product.productCategoryId}>{product.name}</option>
-      ))}
-    </select>
+    <h1>User List 12333</h1>
+     
   </div>
   )
 }
